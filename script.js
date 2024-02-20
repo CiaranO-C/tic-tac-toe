d06a934ec2c03eec6b2859b2369ba3e96f3eba70
CiaranOC <129303811+CiaranO-C@users.noreply.github.com>
2024-02-20 12:28:51 +0000

    play single game logic put inside dedicated function.
    play again? confirm at end, alter setMarkers
    function, allowing players option to swap markers

--- script.js
+++ script.js
@@ -16,11 +16,9 @@ function createPlayer() {
 
     const setMarker = (choice) => {
         const choiceCaps = choice.toUpperCase();
-        if (!marker && choiceCaps === 'X' || choiceCaps === 'O') {
+        if (choiceCaps === 'X' || choiceCaps === 'O') {
             marker = choiceCaps;
             (marker === 'X') ? turn = true : turn = false;
-        } else if (marker) {
-            console.log(`${playerName} is already ${marker}!`);
         } else {
             console.log('Whoops! Try again, enter either X or O!');
         };
@@ -30,7 +28,7 @@ function createPlayer() {
 
     const win = () => score++;
 
-    const getScore = () => console.log(score);
+    const getScore = () => score;
 
     const isTurn = () => turn;
 
@@ -47,26 +45,28 @@ const gameBoard = (() => {
 
     const hasWon = () => winner;
 
-    const generateBoard = () => {
-        for (let i = 0; i < rows; i++) {
-            board.push([]);
-            for (let j = 0; j < cols; j++) {
-                board[i].push('');
+    const generateBoard = (shouldClear = false) => {
+        //either generates intial board, or clears existing board
+        if (!shouldClear) {
+            for (let i = 0; i < rows; i++) {
+                board.push([]);
+                for (let j = 0; j < cols; j++) {
+                    board[i].push('');
+                };
             };
+        } else {
+            for (let i = 0; i < rows; i++) {
+                for (let j = 0; j < cols; j++) {
+                    board[i][j] = '';
+                };
+            };
+            winner = false;
         };
         return board;
     };
 
     const getBoard = () => {
         return board;
-    };
-
-    const clearBoard = () => {
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < cols; j++) {
-                board[i][j] = '';
-            };
-        };
     };
 
     const updateBoard = (i, j, marker) => {
@@ -78,8 +78,6 @@ const gameBoard = (() => {
     };
 
     const validateMove = (i, j) => {
-        console.log(i);
-        console.log(j);
         if (i > 2 || j > 2) {
             console.log('Please enter valid co-ords!')
             return false;
@@ -117,28 +115,35 @@ const gameBoard = (() => {
 
         function checkDiag() {
             const marker = board[1][1];
-            if (!marker) return;
-            for (let i = 0; i < 3; i++) {
-                const j = i;
 
-                if (board[i][j] !== marker) {
-                    return;
-                } else if (i === 2) {
-                    winner = true;
-                    return;
+            function diagOne() {
+                if (!marker) return;
+                for (let i = 0; i < 3; i++) {
+                    const j = i;
+
+                    if (board[i][j] !== marker) {
+                        return;
+                    } else if (i === 2) {
+                        winner = true;
+                        return;
+                    };
                 };
             };
 
-            for (let i = 0; i < 3; i++) {
-                let j = 2 - i;
+            function diagTwo() {
+                for (let i = 0; i < 3; i++) {
+                    let j = 2 - i;
 
-                if (board[i][j] !== marker) {
-                    return;
-                } else if (i === 2) {
-                    winner = true;
-                    return;
+                    if (board[i][j] !== marker) {
+                        return;
+                    } else if (i === 2) {
+                        winner = true;
+                        return;
+                    };
                 };
             };
+            diagOne();
+            diagTwo();
         };
 
         checkRows();
@@ -146,35 +151,53 @@ const gameBoard = (() => {
         checkDiag();
     };
 
-    return { hasWon, getBoard, generateBoard, clearBoard, updateBoard, validateMove, checkWinner };
+    return { hasWon, getBoard, generateBoard, updateBoard, validateMove, checkWinner };
 })();
 
 
 const gameControl = () => {
-
     const playerOne = createPlayer();
-
-    playerOne.setName('Harvey');
-
-    console.log(`Hello, ${playerOne.getName()}!`);
-
-    playerOne.setMarker('X');
-    console.log(`${playerOne.getName()} has chosen ${playerOne.getMarker()}!`);
-
     const playerTwo = createPlayer();
-    playerTwo.setName('Wilbur');
-    console.log(`Hello, ${playerTwo.getName()}!`);
-    if (playerOne.getMarker() === 'X') {
-        playerTwo.setMarker('O');
-    } else {
-        playerTwo.setMarker('X');
+
+    function setNames(player, name) {
+        player.setName(name);
+        console.log(`Hello, ${player.getName()}!`);
+    }
+
+    setNames(playerOne, 'Beep');
+    setNames(playerTwo, 'Boop');
+
+    function setMarkers() {
+        //if player one has no marker, must be first round
+        if (!playerOne.getMarker()) {
+            playerOne.setMarker('X');
+            console.log(`${playerOne.getName()} has chosen ${playerOne.getMarker()}!`);
+
+            if (playerOne.getMarker() === 'X') {
+                playerTwo.setMarker('O');
+            } else {
+                playerTwo.setMarker('X');
+            };
+            console.log(`${playerTwo.getName()}'s marker is ${playerTwo.getMarker()}!`);
+
+            //play again selected 
+        } else {
+            const shouldChange = confirm('Would you like to change markers?');
+            if (shouldChange) {
+                const token = playerOne.getMarker();
+                playerOne.setMarker(playerTwo.getMarker());
+                playerTwo.setMarker(token);
+                console.log(`You swapped markers! ${playerOne.getName()} is now ${playerOne.getMarker()}... and ${playerTwo.getName()} is ${playerTwo.getMarker()}!`);
+            };
+        };
     };
-    console.log(`${playerTwo.getName()}'s marker is ${playerTwo.getMarker()}!`);
+
+    setMarkers();
 
     gameBoard.generateBoard();
 
-    let moveCount = 1;
-    function switchPlayer() {
+
+    function switchTurn() {
         if (playerOne.isTurn()) {
             playerOne.setTurn(false);
             playerTwo.setTurn(true);
@@ -200,38 +223,70 @@ const gameControl = () => {
     };
 
     console.log('Let the game begin!');
-    console.table(gameBoard.getBoard());
 
-    while (!gameBoard.hasWon()) {
-        getMove();
+    playGame();
 
-        if (gameBoard.validateMove(row, col)) {
-            gameBoard.updateBoard(row, col, currentPlayer().getMarker());
-        };
+    function playGame() {
 
         console.table(gameBoard.getBoard());
+        let moveCount = 1;
 
-        if (moveCount > 4) {
-            gameBoard.checkWinner();
-        }
+        while (!gameBoard.hasWon()) {
 
-        moveCount++;
-        switchPlayer();
+            do {
+                getMove();
+            } while (!gameBoard.validateMove(row, col));
+
+            gameBoard.updateBoard(row, col, currentPlayer().getMarker());
+
+            console.table(gameBoard.getBoard());
+
+            if (moveCount > 4) {
+                gameBoard.checkWinner();
+            }
+
+            moveCount++;
+            switchTurn();
+        };
+
     };
+
+    function winsScores() {
+        //set turn back to winning player
+        switchTurn();
+
+
+        if (playerOne.isTurn()) {
+            console.log(`${playerOne.getName()} Wins!!!`);
+            playerOne.win();
+        } else {
+            console.log(`${playerTwo.getName()} Wins!!!`);
+            playerTwo.win();
+        };
+
+        console.log(`*---*  ${playerOne.getName()}'s score: ${playerOne.getScore()}  *---*`);
+        console.log(`*---*  ${playerTwo.getName()}'s score: ${playerTwo.getScore()}  *---*`);
+    };
+
+    winsScores();
+
+    let again = confirm('Play Again???');
+    while (again) {
+        setMarkers();
+
+        gameBoard.generateBoard('clear');
+
+        console.log(`Let round ${playerOne.getScore() + playerTwo.getScore() + 1} begin!`)
+
+        playGame();
+        winsScores();
+
+        again = confirm('Play Again???');
+    };
+
+    console.log('Thanks for playing!');
 };
 
 
 gameControl();
 
-/*
-game controller
-
-loop until winner=true
-get move (player1 || player2)
-updateBoard()
-totalMoves++ 
-if(totalMoves > 4){
-    checkWinner
-}
-swap values at end of turn
-*/
